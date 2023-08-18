113c0011ef693a63fb586f6b53cf1287ab17d613
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-18 14:16:17 +0300

    Completed

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+function Links(props){
+    return(
+        <div id="links">
+            <h1>Links</h1>
+            <h2>{props.links}</h2>
+        </div>
+    )
+}
+
+export default Links
